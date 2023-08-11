e1cb2280c6a7b6ccef426f1e44af045ff7939f22
Alireza Ziaee <alrz1999@gmail.com>
2023-08-11 20:10:51 +0330

    Self Encapsulate Field for Rule class

--- MiniJava/src/main/java/parser/Rule.java
+++ MiniJava/src/main/java/parser/Rule.java
@@ -9,35 +9,59 @@ import java.util.ArrayList;
  */
 
 public class Rule {
-    public NonTerminal LHS;
-    public ArrayList<GrammarSymbol> RHS;
-    public int semanticAction;
+    private NonTerminal LHS;
+    private ArrayList<GrammarSymbol> RHS;
+    private int semanticAction;
 
     public Rule(String stringRule) {
         int index = stringRule.indexOf("#");
         if (index != -1) {
             try {
-                semanticAction = Integer.parseInt(stringRule.substring(index + 1));
+                setSemanticAction(Integer.parseInt(stringRule.substring(index + 1)));
             } catch (NumberFormatException ex) {
-                semanticAction = 0;
+                setSemanticAction(0);
             }
             stringRule = stringRule.substring(0, index);
         } else {
-            semanticAction = 0;
+            setSemanticAction(0);
         }
         String[] splited = stringRule.split("->");
-        LHS = NonTerminal.valueOf(splited[0]);
-        RHS = new ArrayList<>();
+        setLHS(NonTerminal.valueOf(splited[0]));
+        setRHS(new ArrayList<>());
         if (splited.length > 1) {
             String[] RHSs = splited[1].split(" ");
             for (String s : RHSs) {
                 try {
-                    RHS.add(new NonTerminalGrammarSymbol(NonTerminal.valueOf(s)));
+                    getRHS().add(new NonTerminalGrammarSymbol(NonTerminal.valueOf(s)));
                 } catch (Exception e) {
-                    RHS.add(new TerminalGrammarSymbol(new Token(Token.getTypeFormString(s), s)));
+                    getRHS().add(new TerminalGrammarSymbol(new Token(Token.getTypeFormString(s), s)));
                 }
             }
         }
+    }
+
+    public NonTerminal getLHS() {
+        return LHS;
+    }
+
+    public void setLHS(NonTerminal LHS) {
+        this.LHS = LHS;
+    }
+
+    public ArrayList<GrammarSymbol> getRHS() {
+        return RHS;
+    }
+
+    public void setRHS(ArrayList<GrammarSymbol> RHS) {
+        this.RHS = RHS;
+    }
+
+    public int getSemanticAction() {
+        return semanticAction;
+    }
+
+    public void setSemanticAction(int semanticAction) {
+        this.semanticAction = semanticAction;
     }
 }
 
@@ -46,27 +70,43 @@ interface GrammarSymbol {
 }
 
 class TerminalGrammarSymbol implements GrammarSymbol {
-    public Token terminal;
+    private Token terminal;
 
     public TerminalGrammarSymbol(Token terminal) {
-        this.terminal = terminal;
+        this.setTerminal(terminal);
     }
 
     @Override
     public boolean isTerminal() {
         return true;
     }
+
+    public Token getTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(Token terminal) {
+        this.terminal = terminal;
+    }
 }
 
 class NonTerminalGrammarSymbol implements GrammarSymbol {
-    public NonTerminal nonTerminal;
+    private NonTerminal nonTerminal;
 
     public NonTerminalGrammarSymbol(NonTerminal nonTerminal) {
-        this.nonTerminal = nonTerminal;
+        this.setNonTerminal(nonTerminal);
     }
 
     @Override
     public boolean isTerminal() {
         return false;
+    }
+
+    public NonTerminal getNonTerminal() {
+        return nonTerminal;
+    }
+
+    public void setNonTerminal(NonTerminal nonTerminal) {
+        this.nonTerminal = nonTerminal;
     }
 }
